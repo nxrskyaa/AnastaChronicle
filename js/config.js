b6d91554ee1caa7a2643444cb81cb21d25e8c32a
nxrskyaa <Nxrskyaa@gmail.com>
2026-07-10 14:40:03 +0800

    v16b: update multiplayer tunnel URL

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Multiplayer config. Set MULTIPLAYER_ENABLED=true and point SERVER_URL at your
 // deployed Colyseus server (wss:// for HTTPS tunnel, ws:// for local).
 export const MULTIPLAYER_ENABLED = true;
-export const SERVER_URL = "wss://2a7a7e08cea0f8c0-43-156-92-8.serveousercontent.com";
+export const SERVER_URL = "wss://230536188420f8ac-43-156-92-8.serveousercontent.com";
