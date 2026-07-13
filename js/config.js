e457404ddfff9d75d6ce72a2376ff4b94dac826b
nxrskyaa <Nxrskyaa@gmail.com>
2026-07-13 10:31:50 +0800

    v20c: VPS named tunnel URL (permanent anasta-vps-4c69d280.cfargotunnel.com)

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Multiplayer config. Set MULTIPLAYER_ENABLED=true and point SERVER_URL at your
 // deployed Colyseus server (wss:// for HTTPS tunnel, ws:// for local).
 export const MULTIPLAYER_ENABLED = true;
-export const SERVER_URL = "wss://descriptions-density-basement-birthday.trycloudflare.com";
+export const SERVER_URL = "wss://anasta-vps-4c69d280.cfargotunnel.com";
